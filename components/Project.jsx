5d50a15719b6e5d04b3557255908ef666e847c55
Mouiz Ahmed <mouiza@my.yorku.ca>
2025-05-21 01:27:53 -0400

    remove project dates

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,14 +1,13 @@
 import Link from 'next/link'
 import Grid from '@mui/material/Grid'
 
-const Project = ({ projectLink, title, tags, description, date }) => {
+const Project = ({ projectLink, title, tags, description }) => {
     return (
         <Grid item xs={9} sm={9} md={9} lg={9} xl={9}>
             <Link href={projectLink}>
                 <div className="hover:bg-[#d5d5d1] dark:hover:bg-[#1f2937] p-2 rounded-lg transition-all duration-200">
                     <div className="flex items-center justify-between">
                         <h3>{title}</h3>
-                        <h3>{date}</h3>
                     </div>
                     <p className="text-sm text-gray-600 dark:text-gray-400">{tags.join(" • ")}</p>
                     <p className="mt-2">{description}</p>
